1af6a8a1f6d6c7eb014cd8ad48d99d8249d29c14
Alialaa122 <133665586+Alialaa122@users.noreply.github.com>
2025-08-29 03:47:48 +0300

    Time: 213 ms (69.13%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT product_name,SUM(unit) AS unit
+FROM products
+INNER JOIN orders
+USING(product_id)
+WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY product_name
+HAVING SUM(unit)>=100 
+
+
