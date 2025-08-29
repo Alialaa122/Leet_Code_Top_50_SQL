dab5236b4fb16e143855ca50d59bf53529024997
Alialaa122 <133665586+Alialaa122@users.noreply.github.com>
2025-08-30 02:12:11 +0300

    Time: 221 ms (85.14%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,20 @@
+-- Write your PostgreSQL query statement below
+
+SELECT query_name,
+       ROUND(SUM(rating *1.0 / position) / COUNT(*) , 2 ) AS quality,
+       ROUND(COUNT(CASE WHEN rating < 3 THEN 1 END ) * 100.0 / COUNT(*) , 2 ) AS poor_query_percentage
+FROM queries
+GROUP BY query_name 
+
+-- YOU CAN USE CASE STATEMENT IN AGGREGATE FUNCTIONS
+
+
+ 
+
+ 
+
+
+
+
+
+
