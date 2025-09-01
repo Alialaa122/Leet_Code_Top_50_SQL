c185f51eff9a3ea82bd30726af9a586e4041ce7c
Alialaa122 <133665586+Alialaa122@users.noreply.github.com>
2025-09-01 22:57:39 +0300

    Time: 179 ms (48.42%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,11 +1,23 @@
 -- Write your PostgreSQL query statement below
-WITH cte1 AS (SELECT machine_id, 
-                    SUM(CASE WHEN activity_type  = 'end' THEN timestamp END) - SUM(CASE WHEN activity_type  = 'start' THEN timestamp END) AS time_to_complete_process,
-             COUNT(DISTINCT process_id) AS total_processes
-             FROM activity
-             GROUP BY machine_id )
+-- THIS is my solution but has bad time complexity 
+-- WITH cte1 AS (SELECT machine_id, 
+--                     SUM(CASE WHEN activity_type  = 'end' THEN timestamp END) - SUM(CASE WHEN activity_type  = 'start' THEN timestamp END) AS time_to_complete_process,
+--              COUNT(DISTINCT process_id) AS total_processes
+--              FROM activity
+--              GROUP BY machine_id )
 
-SELECT machine_id , ROUND((time_to_complete_process/total_processes)::numeric,3) AS processing_time 
-FROM cte1
-GROUP BY machine_id,time_to_complete_process,total_processes
+-- SELECT machine_id , ROUND((time_to_complete_process/total_processes)::numeric,3) AS processing_time 
+-- FROM cte1
+-- GROUP BY machine_id,time_to_complete_process,total_processes
+
+-- ANOTHER ONE 
+SELECT a1.machine_id , ROUND(AVG(a2.timestamp-a1.timestamp)::numeric,3) AS processing_time  
+FROM activity AS a1 
+   JOIN activity AS a2 
+      ON  a1.machine_id=a2.machine_id
+      AND a1.process_id=a2.process_id
+      AND a1.activity_type='start'
+      AND a2.activity_type='end'
+GROUP BY a1.machine_id
+
 
