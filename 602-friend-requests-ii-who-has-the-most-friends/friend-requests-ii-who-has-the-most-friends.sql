f815e9562e4a2d177f5e53b05e1a055136610ea3
Alialaa122 <133665586+Alialaa122@users.noreply.github.com>
2025-09-04 05:47:53 +0300

    Time: 173 ms (89.74%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,32 +1,35 @@
 -- Write your PostgreSQL query statement below
 
+-- MY SOLUTION 
 
--- WITH cte1 AS (SELECT requester_id ,COUNT(*) AS friends1
---               FROM RequestAccepted 
---               GROUP BY requester_id ),
+WITH c1 AS (SELECT requester_id AS user_id ,COUNT(*) AS friends1
+              FROM RequestAccepted 
+              GROUP BY requester_id ),
 
--- cte2 AS(SELECT accepter_id ,COUNT(*) AS friends2
---         FROM RequestAccepted 
---         GROUP BY accepter_id )
+c2 AS(SELECT accepter_id AS user_id  ,COUNT(*) AS friends2
+        FROM RequestAccepted 
+        GROUP BY accepter_id )
 
--- select cte1.requester_id AS id , SUM(cte1.friends1+cte2.friends2) AS num 
--- FROM cte1
--- JOIN cte2 on cte1.requester_id=cte2.accepter_id
--- GROUP BY cte1.requester_id
--- ORDER BY num DESC 
--- LIMIT 1 
-
-
-WITH cte1 AS (SELECT requester_id AS id 
-           FROM RequestAccepted
-
-           UNION ALL
-
-           SELECT accepter_id AS id 
-           FROM RequestAccepted   )
-
-SELECT id , COUNT(*) AS num
-FROM cte1 
-GROUP BY id 
+SELECT COALESCE(c1.user_id,c2.user_id,0) AS id,
+       COALESCE(c1.friends1,0) + COALESCE(c2.friends2,0) AS num 
+FROM c1
+FULL OUTER JOIN c2 on c1.user_id=c2.user_id 
 ORDER BY num DESC
-LIMIT 1 
+LIMIT 1  
+
+
+
+
+-- WITH cte1 AS (SELECT requester_id AS id 
+--            FROM RequestAccepted
+
+--            UNION ALL
+
+--            SELECT accepter_id AS id 
+--            FROM RequestAccepted   )
+
+-- SELECT id , COUNT(*) AS num
+-- FROM cte1 
+-- GROUP BY id 
+-- ORDER BY num DESC
+-- LIMIT 1 
