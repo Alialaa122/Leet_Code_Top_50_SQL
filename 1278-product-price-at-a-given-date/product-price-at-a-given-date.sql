f42559cd7bd68dc00c5cc4b83528814bfbb3412f
Ali Alaa Ali Hassan Mohammed <133665586+Alialaa122@users.noreply.github.com>
2025-09-06 00:28:02 +0300

    Time: 343 ms (88.92%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,16 +1,6 @@
 -- Write your PostgreSQL query statement below
 
--- WITH cte1 AS (SELECT product_id , new_price , change_date 
---               FROM products
---               WHERE change_date <= '2019-08-16' ) , 
 
--- cte2 AS (SELECT *
---          FROM products
---          LEFT JOIN cte1 ON cte1.change_date=products.change_date 
---                         AND cte1.product_id=products.product_id )
-
-
--- SELECT products.product_id , CASE WHEN change_date IS NULL THEN 10 
 
 WITH cte1 AS (SELECT product_id , new_price , ROW_NUMBER()OVER(PARTITION BY product_id
                                                  ORDER BY change_date DESC ) AS rank
