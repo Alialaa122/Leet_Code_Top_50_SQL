7643a5342a33028274fb00df8948eee4a2d06dd7
Ali Alaa Ali Hassan Mohammed <133665586+Alialaa122@users.noreply.github.com>
2025-09-07 12:12:41 +0300

    Time: 191 ms (97.88%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -2,6 +2,7 @@
 WITH cte1 AS (SELECT id,salary, DENSE_RANK()OVER(ORDER BY salary DESC)  AS rank
               FROM employee ) 
 
+-- USING MAX() will return null if the value doesnot exist 
 SELECT MAX(DISTINCT salary) AS SecondHighestSalary 
 FROM cte1 
 WHERE rank = 2 
